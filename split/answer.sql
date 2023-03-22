ba331f4068ae9be0f60fc39c379a988e7f07f0aa
dahaynes <dahaynes@umn.edu>
2023-03-22 14:36:06 -0500

    Updated formatting

--- split/answer.sql
+++ split/answer.sql
@@ -1,8 +1,12 @@
 WITH point as
 (
-SELECT ST_SETSRID(  ST_MakeLine(
- ST_MakePoint(-97.239,(43.499+((49.384-43.499)/2))),
-ST_MakePoint(-89.492,(43.499+((49.384-43.499)/2)))  ),4326 ) as geom,
+SELECT ST_SETSRID(  
+            -- ST_SETRID(geom, SRID=INT)
+            ST_MakeLine(
+                -- geom1, geom2 = geom
+                    ST_MakePoint(-97.239,(43.499+((49.384-43.499)/2))),
+                    ST_MakePoint(-89.492,(43.499+((49.384-43.499)/2)))  ),4326 
+                ) as geom,
 
 ST_Makeline(ST_GeomFromText('POINT( -97.237 46.4415)',4326), ST_GeomFromText('POINT( -89.492 46.4415)',4326)) as line
 ), polygon_dump as
